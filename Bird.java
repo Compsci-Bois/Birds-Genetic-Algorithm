f09fc3b4e2a026a691367281ed935b654a31e423
Gabert205 <56002135+Gabert205@users.noreply.github.com>
2019-09-30 22:30:52 -0500

    Add files via upload

--- Bird.java
+++ Bird.java
@@ -2,5 +2,116 @@ public class Bird {
     private Vector pos;//the current position of the Bird
     private Vector vel;//the current velocity of the Bird
     private Vector acc;//the current acceleration of the Bird
-    private Vector[] accelerations;//the motions added to the Bird at each frame update
+    private Vector[] accelerations;//the motions added to the Bird at each frame update - acts as genes
+    private double fitness;
+
+    /**
+     * This constructor is meant mostly for creating your initial population
+     *
+     * @param startingX - the starting x-coordinate of the bird
+     * @param startingY - the starting y-coordinate of the bird
+     * @param runLength - the number of frames that the Bird will last
+     */
+
+    public Bird(double startingX, double startingY, int runLength){
+        pos = new Vector(startingX, startingY);
+        vel = new Vector();
+        acc = new Vector();
+        accelerations = new Vector[runLength];
+        fitness = 0;
+        //make all starting Birds have random genes (accelerations)
+    }
+
+    /**
+     * This constructor is meant more for breeding
+     *
+     * @param startingX - the starting x-coordinate of the bird
+     * @param startingY - the starting y-coordinate of the bird
+     * @param accelerations - the pre-determined accelerations/genes that this Bird will have
+     */
+
+    public Bird(double startingX, double startingY, Vector[] accelerations){
+        pos = new Vector(startingX, startingY);
+        vel = new Vector();
+        acc = new Vector();
+        this.accelerations = accelerations;
+    }
+
+    //==================================================================================================================
+
+    //region Gets and Sets
+    public Vector getPos() {
+        return pos;
+    }
+
+    public void setPos(Vector pos) {
+        this.pos = pos;
+    }
+
+    public Vector getVel() {
+        return vel;
+    }
+
+    public void setVel(Vector vel) {
+        this.vel = vel;
+    }
+
+    public Vector getAcc() {
+        return acc;
+    }
+
+    public void setAcc(Vector acc) {
+        this.acc = acc;
+    }
+
+    public Vector[] getAccelerations() {
+        return accelerations;
+    }
+
+    public void setAccelerations(Vector[] accelerations) {
+        this.accelerations = accelerations;
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    //endregion
+
+    //==================================================================================================================
+
+    //draws the Bird - probably just a small circle
+    public void draw(){
+
+    }
+
+    /**
+     * @param x - the x-coordinate of some point
+     * @param y - the y-coordinate of some point
+     * @return  - the distance between the Bird and the point
+     */
+
+    public double distance(int x, int y){
+        return Math.sqrt( Math.pow(pos.getX() - x, 2) + Math.pow(pos.getY() - y, 2) );
+    }
+
+    /**
+     * This method will be used to create offspring given this Bird and some other mate
+     *
+     * @param mate - the other Bird that acts as a second parent
+     * @return the offspring of the two parent Birds
+     */
+    public Bird mateWith(Bird mate){
+        //randomly select genes from either parent
+
+        //a small chance of mutation (I typically do 1/100, but you can do anything)
+
+        //return the new Bird
+
+        return null;
+    }
 }
--- Population.java
+++ Population.java
@@ -1,2 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Population {
+    private List<Bird> population;//a list that stores all Birds
+    private int generation;//the number of iterations that the entire population has endured
+
+    /**
+     * @param populationSize - the starting Population size
+     * @param startingX - the x coordinate of where the Birds will start
+     * @param startingY - the y coordinate of where the Birds will start
+     * @param runLength - the number of frames that the Birds will run for
+     */
+    public Population(int populationSize, double startingX, double startingY, int runLength){
+        population = new ArrayList<>();
+        for(int i = 0 ; i < populationSize ; i++){
+            population.add(new Bird(startingX, startingY,runLength));
+        }
+    }
+
+    //==================================================================================================================
+
+    /**
+     * Calculates the fitness for all the Birds in population
+     * Run some test for the Birds to get close to a set goal
+     * The fitness needs to be related to the distance to the goal in some way
+     */
+    public void calculateFitness(){
+
+    }
+
+    /**
+     * Kill of the worst 50% of the population (determined by the fitness score)
+     *
+     * P.S. - It isn't a good idea to kill off only the worst 50% - the whole reason genetic algorithms work
+     * is because of random chance through mutations and new gene combinations - it is better for the lower 50%
+     * to have a higher chance of dying off and the upper 50% having a higher chance of surviving, but anyone can die
+     * This promotes a more genetically diverse population because sometimes the sucky genes work really well when
+     * combined with other stuff
+     */
+    public void theReaping(){
+
+    }
+
+    /**
+     * The surviving (and therefore genetically superior) Birds are tired from all the testing and need a break
+     * This method will help them take a break by creating some children
+     * Loop through the surviving population such that surviving Birds mate only with other surviving Birds (NOT NEW ONES)
+      */
+    public void sexPit(){
+
+    }
+
+    /**
+     * Draws all the birds
+     */
+    public void draw(){
+
+    }
 }
